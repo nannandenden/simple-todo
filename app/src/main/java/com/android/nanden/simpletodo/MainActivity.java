35728f536eb861d158f80228043ff36c1f22773f
Nan Den <nannandenden@gmail.com>
2017-08-22 10:33:08 -0700

    initial commit

---
+++ app/src/main/java/com/android/nanden/simpletodo/MainActivity.java
@@ -0,0 +1,104 @@
+package com.android.nanden.simpletodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int EIDT_TEXT_REQUEST = 2;
+
+    ListView lvItems;
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        setupListViewListner();
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    private void setupListViewListner() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                items.remove(pos);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String currentItem = items.get(i);
+                Intent editItemIntent = new Intent(MainActivity.this, EditItemActivity.class);
+                editItemIntent.putExtra("EDIT_ITEM", currentItem);
+                editItemIntent.putExtra("EDIT_ITEM_POSITION", i);
+                startActivityForResult(editItemIntent, EIDT_TEXT_REQUEST);
+            }
+        });
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == EIDT_TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                int position = data.getIntExtra("SAVE_ITEM_POSITION", -1);
+                String updateItem = data.getStringExtra("SAVE_ITEM");
+                items.set(position, updateItem);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+            }
+        }
+    }
+}
