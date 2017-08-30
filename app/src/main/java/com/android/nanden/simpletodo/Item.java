6e2aaae246627c14b3c44a7091f3cb15251d4169
Nan Den <nannandenden@gmail.com>
2017-08-30 14:31:55 -0700

    update to EditItemDialogFragment instead of EditItemActivity.

--- app/src/main/java/com/android/nanden/simpletodo/Item.java
+++ app/src/main/java/com/android/nanden/simpletodo/Item.java
@@ -1,6 +1,9 @@
 package com.android.nanden.simpletodo;
 
-public class Item {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Item implements Parcelable {
 
     public Long _id;
     public String itemName;
@@ -25,4 +28,32 @@ public class Item {
         return _id;
     }
 
+
+    protected Item(Parcel in) {
+        itemName = in.readString();
+        _id = in.readLong();
+    }
+
+    public static final Creator<Item> CREATOR = new Creator<Item>() {
+        @Override
+        public Item createFromParcel(Parcel in) {
+            return new Item(in);
+        }
+
+        @Override
+        public Item[] newArray(int size) {
+            return new Item[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(itemName);
+        parcel.writeLong(_id);
+    }
 }
--- app/src/main/java/com/android/nanden/simpletodo/MainActivity.java
+++ app/src/main/java/com/android/nanden/simpletodo/MainActivity.java
@@ -1,8 +1,8 @@
 package com.android.nanden.simpletodo;
 
-import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,9 +17,8 @@ import nl.qbusict.cupboard.QueryResultIterable;
 
 import static nl.qbusict.cupboard.CupboardFactory.cupboard;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final int EDIT_TEXT_REQUEST = 2;
+public class MainActivity extends AppCompatActivity implements EditItemDialogFragment
+        .EditItemDialogListener {
 
     static SQLiteDatabase db;
 
@@ -46,19 +45,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == EDIT_TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                int position = data.getIntExtra("SAVE_ITEM_POSITION", -1);
-                String updateItem = data.getStringExtra("SAVE_ITEM");
-                Item item = items.get(position);
-                item.setItemName(updateItem);
-                cupboard().withDatabase(db).put(item);
-                itemsAdapter.notifyDataSetChanged();
-            }
-        }
+    public void onFinishEditDialog(Item item) {
+        cupboard().withDatabase(db).put(item);
+        itemsAdapter.notifyDataSetChanged();
     }
 
     public void onAddItem(View v) {
@@ -91,10 +80,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Item currentItem = items.get(i);
-                Intent editItemIntent = new Intent(MainActivity.this, EditItemActivity.class);
-                editItemIntent.putExtra("EDIT_ITEM", currentItem.getItemName());
-                editItemIntent.putExtra("EDIT_ITEM_POSITION", i);
-                startActivityForResult(editItemIntent, EDIT_TEXT_REQUEST);
+                FragmentManager fm = getSupportFragmentManager();
+                EditItemDialogFragment editItemDialogFragment = new EditItemDialogFragment()
+                        .newInstance(currentItem);
+                editItemDialogFragment.show(fm, "fragment_edit_item");
             }
         });
     }
